5821fff00f103503fe6c18e01a1fd8e71fe7c051
Krzysztof Szumny <noisy.pl@gmail.com>
2021-05-20 13:23:32 +0200

    fix for multiple components not rendered

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -1,5 +1,4 @@
 import ProjectThumbnail from "./components/projectThumbnail.js";
-// import Reksio from "./components/reksio.js";
 import Testimonial from "./components/testimonial.js";
 import db from "./db.js";
 
@@ -13,7 +12,3 @@ const app = Vue.createApp({
 app.component("project-thumbnail", ProjectThumbnail);
 app.component("testimonial", Testimonial);
 app.mount("#app");
-
-// const app2 = Vue.createApp({});
-// app2.component("reksio", Reksio);
-// app2.mount("#app2");
