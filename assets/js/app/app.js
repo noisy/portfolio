9ee120ea7a6b4ff4fcda673dcd05d23296d0107a
Krzysztof Szumny <noisy.pl@gmail.com>
2021-05-20 16:54:52 +0200

    testing WebHistory

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -2,6 +2,30 @@ import ProjectThumbnail from "./components/projectThumbnail.js";
 import Testimonial from "./components/testimonial.js";
 import db from "./db.js";
 
+// 1. Define route components.
+// These can be imported from other files
+const Home = { template: "<div>Home</div>" };
+const About = { template: "<div>About</div>" };
+
+// 2. Define some routes
+// Each route should map to a component.
+// We'll talk about nested routes later.
+const routes = [
+  { path: "/", component: Home },
+  { path: "/about", component: About },
+];
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = VueRouter.createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: VueRouter.createWebHistory(),
+  routes, // short for `routes: routes`
+});
+
+window.route = VueRouter.useRoute();
+
 const app = Vue.createApp({
   data() {
     return {
@@ -9,6 +33,7 @@ const app = Vue.createApp({
     };
   },
 });
+app.use(router);
 app.component("project-thumbnail", ProjectThumbnail);
 app.component("testimonial", Testimonial);
 app.mount("#app");
