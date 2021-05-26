0e3275bb4a4d455a5b7a644bf5c132530a1715ef
Krzysztof Szumny <noisy.pl@gmail.com>
2021-05-26 14:25:23 +0200

    projectsGrid

---
+++ assets/js/app/components/projectsGrid.js
@@ -0,0 +1,28 @@
+import ProjectThumbnail from "./projectThumbnail.js";
+import { setupIsotopeFilters } from "../../isotope-custom.js";
+
+export default {
+  components: { ProjectThumbnail },
+  props: ["projects"],
+  template: `
+    <div class="container">
+        <div class="project-cards row mb-5 isotope">
+            <project-thumbnail
+              v-for="project in projects"
+              class="isotope-item"
+              :name="project.name"
+              :slug="project.slug"
+              :thumbnail="project.thumbnail"
+              :summary-title="project.summaryTitle"
+              :summary="project.summary"
+              :tags="project.tags"
+              :badges="project.badges"
+              :thumbnail-logo="project.thumbnailLogo" />
+
+        </div>
+    </div>
+  `,
+  setup(props) {
+    Vue.onMounted(setupIsotopeFilters);
+  },
+};
