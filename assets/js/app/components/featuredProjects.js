7f434ceae86151e1e55306a30bb2a8b6098dd843
Krzysztof Szumny <noisy.pl@gmail.com>
2021-05-27 14:53:20 +0200

    featuredProjects

--- assets/js/app/components/featuredProjects.js
+++ assets/js/app/components/featuredProjects.js
@@ -1,18 +1,13 @@
-import ProjectThumbnail from "./projectThumbnail.js";
+import ProjectsGrid from "./projectsGrid.js";
 import { useFeaturedProjects } from "../composables/useFeaturedProjects.js";
 
 export default {
-  components: { ProjectThumbnail },
+  components: { ProjectsGrid },
   template: `
     <section class="section-featured-projects py-5">
         <div class="container">
             <h3 class="section-title font-weight-bold text-center mb-5">Featured Projects</h3>
-            <div class="project-cards row mb-5">
-            <project-thumbnail v-for="project in projects" :name="project.name" :thumbnail="project.thumbnail"
-                    :summary-title="project.summaryTitle" :summary="project.summary" :tags="project.tags"
-                    :badges="project.badges" :thumbnail-logo="project.thumbnailLogo"></project-thumbnail>
-
-            </div>
+            <ProjectsGrid :projects="projects"></ProjectsGrid>
             <div class="text-center">
                 <router-link class="btn btn-primary" to="/projects">View all projects</router-link>
             </div>
