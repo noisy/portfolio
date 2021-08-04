f350010c63727aba846c5fcab5382fb747876e97
Krzysztof Szumny <noisy.pl@gmail.com>
2021-08-04 23:15:14 +0200

    draft Blog.vue

---
+++ src/views/Blog.vue
@@ -0,0 +1,13 @@
+<template>
+  <BlogHeader />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { BlogHeader } from "@/components";
+
+export default defineComponent({
+  name: "Blog",
+  components: { BlogHeader },
+});
+</script>
