452effd9095a41ecb2dd0e8e5900e1ec6442c410
Krzysztof Szumny <noisy.pl@gmail.com>
2021-08-04 23:55:10 +0200

    font-awesome icons

--- src/components/BlogHeader.vue
+++ src/components/BlogHeader.vue
@@ -12,11 +12,10 @@
               align-self-center
               mb-2 mb-lg-0
             "
-            src="assets/images/profile.png"
+            src="/images/profile.png"
             alt=""
           />
         </div>
-        <!--//col-->
         <div class="col align-self-center">
           <h2 class="page-heading mb-2">Krzysztof Szumny's Blog</h2>
           <div class="page-heading-tagline mb-3">
@@ -25,43 +24,69 @@
             channels below:
           </div>
           <ul class="social-list-square list-inline mb-0">
-            <li class="list-inline-item mb-3 mb-lg-0">
-              <a href="#"><i class="fab fa-twitter fa-fw text-white"></i></a>
+            <li
+              v-if="links.twitterProfile"
+              class="list-inline-item mb-3 mb-lg-0"
+            >
+              <a :href="links.twitterProfile">
+                <font-awesome-icon
+                  :icon="['fab', 'twitter']"
+                  class="text-white"
+                />
+              </a>
             </li>
 
-            <li class="list-inline-item mb-3 mb-lg-0">
-              <a href="#"
-                ><i class="fab fa-linkedin-in fa-fw text-white"></i
-              ></a>
+            <li
+              v-if="links.linkedinProfile"
+              class="list-inline-item mb-3 mb-lg-0"
+            >
+              <a :href="links.linkedinProfile">
+                <font-awesome-icon
+                  :icon="['fab', 'linkedin']"
+                  class="text-white"
+                />
+              </a>
             </li>
-            <li class="list-inline-item mb-3 mb-lg-0">
-              <a href="#"><i class="fab fa-github-alt fa-fw text-white"></i></a>
+
+            <li
+              v-if="links.githubProfile"
+              class="list-inline-item mb-3 mb-lg-0"
+            >
+              <a :href="links.githubProfile">
+                <font-awesome-icon
+                  :icon="['fab', 'github']"
+                  class="text-white"
+                />
+              </a>
             </li>
-            <li class="list-inline-item mb-3 mb-lg-0">
-              <a href="#"
-                ><i class="fab fa-stack-overflow fa-fw text-white"></i
-              ></a>
-            </li>
-            <li class="list-inline-item mb-3 mb-lg-0">
-              <a href="#"><i class="fab fa-medium-m fa-fw text-white"></i></a>
-            </li>
-            <li class="list-inline-item mb-3 mb-lg-0">
-              <a href="#"><i class="fab fa-codepen fa-fw text-white"></i></a>
+
+            <li
+              v-if="links.stackOverflowProfile"
+              class="list-inline-item mb-3 mb-lg-0"
+            >
+              <a :href="links.stackOverflowProfile">
+                <font-awesome-icon
+                  :icon="['fab', 'stack-overflow']"
+                  class="text-white"
+                />
+              </a>
             </li>
           </ul>
-          <!--//social-list-->
         </div>
-        <!--//col-->
       </div>
     </div>
-    <!--//container-->
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { useDB } from "@/composables";
 
 export default defineComponent({
   name: "BlogPostHeader",
+  setup() {
+    const { links } = useDB();
+    return { links };
+  },
 });
 </script>
