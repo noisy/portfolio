5dc48b9bb58538aa9497bbae820554dca94093c5
Krzysztof Szumny <noisy.pl@gmail.com>
2022-03-22 22:24:17 +0100

    updated video about seerene

--- src/views/projects/Seerene.vue
+++ src/views/projects/Seerene.vue
@@ -19,7 +19,7 @@
     <p class="video-container">
       <iframe
         class="video"
-        src="https://www.youtube.com/embed/zk3_lOGBlp8"
+        src="https://www.youtube.com/embed/6dtqTChsfN0"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
