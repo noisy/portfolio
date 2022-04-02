00051667cc14eb140e8bbab573ffceae0206450f
Krzysztof Szumny <noisy.pl@gmail.com>
2022-04-02 02:01:52 +0200

    adding missing imports to Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,15 @@
 </template>
 
 <script setup lang="ts">
+import {
+  Bio,
+  Companies,
+  FeaturedIn,
+  FeaturedProjects,
+  LatestBlogPosts,
+  SkillsOverview,
+  Testimonials,
+} from "@/components";
 import { useDB } from "@/composables";
 
 const { testimonials } = useDB();
