dfee680f75a4e925ee2298a3a8f657a42d8c794a
Krzysztof Szumny <noisy.pl@gmail.com>
2022-06-29 01:56:54 +0200

    Fix for issue #4 - Footer should always stay on the bottom of the page

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
-  <HireMeFooter />
-  <footer class="footer text-light text-center py-2">
-    <small class="copyright"
+  <footer class="footer text-light text-center">
+    <HireMeFooter />
+    <small class="copyright my-2"
       >Built with Vue.js 3 & Bootstrap 5. ©
       {{ new Date().getFullYear() }} Krzysztof Szumny. All rights
       reserverd.</small
