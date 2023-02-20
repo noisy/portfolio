9fdbf07b5e14e766422d2873c5f015675a6c0f02
Krzysztof Szumny <noisy.pl@gmail.com>
2023-02-20 22:56:50 +0100

    chore: code formatting changed

--- src/modules/filters/filters.ts
+++ src/modules/filters/filters.ts
@@ -14,12 +14,20 @@ export function getDynamicFilters<
     items,
     filterAttributeName
   );
-  const neededFilterTags = [allFilterTag as FilterTag, ...usedFilterTags]
-  const usedFilters = filters.filter(({tag}) => neededFilterTags.includes(tag as FilterTag))
-  const tagsWhichDoNotHavePairInFilters = neededFilterTags.filter((tag)=> !usedFilters.map(({tag})=> tag as FilterTag).includes(tag))
+  const neededFilterTags = [allFilterTag as FilterTag, ...usedFilterTags];
+  const usedFilters = filters.filter(({ tag }) =>
+    neededFilterTags.includes(tag as FilterTag)
+  );
+  const tagsWhichDoNotHavePairInFilters = neededFilterTags.filter(
+    (tag) => !usedFilters.map(({ tag }) => tag as FilterTag).includes(tag)
+  );
 
   if (tagsWhichDoNotHavePairInFilters.length > 0) {
-    throw new Error(`Some tags do not have pair in filters: ${tagsWhichDoNotHavePairInFilters.join(", ")}`)
+    throw new Error(
+      `Some tags do not have pair in filters: ${tagsWhichDoNotHavePairInFilters.join(
+        ", "
+      )}`
+    );
   }
 
   return usedFilters;
