49ef729576532ee42ef488325a3dae5eed0791b5
Krzysztof Szumny <noisy.pl@gmail.com>
2023-03-13 00:33:12 +0100

    ci: cypress runs on 'npm run preview' now and on port 5050

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectId: 'ubrejg',
+  projectId: "ubrejg",
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       return require("./cypress/plugins/index.ts").default(on, config);
     },
-    baseUrl: "http://localhost:3000",
+    baseUrl: "http://localhost:5050",
   },
 });
