5b08cb048eaad7597bd3280b5f49c84ba35aed89
Krzysztof Szumny <noisy.pl@gmail.com>
2023-03-14 23:24:44 +0100

    ci,test: cypress configs splitted into 2 seperate files

--- cypress.config.e2e.ts
+++ cypress.config.e2e.ts
@@ -10,12 +10,4 @@ export default defineConfig({
     baseUrl: "http://localhost:5050",
     specPattern: "cypress/e2e/**/*.cy.ts",
   },
-
-  component: {
-    devServer: {
-      framework: "vue",
-      bundler: "vite",
-    },
-    specPattern: "src/components/**/*.cy.ts",
-  },
 });
