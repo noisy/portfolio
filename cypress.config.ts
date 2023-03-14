435c2325dc29a10dbd78a5f7e72576969d6c962a
Krzysztof Szumny <noisy.pl@gmail.com>
2023-03-14 22:53:22 +0100

    testing running tests with 2 different record keys

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   projectId: "ubrejg",
+
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       return require("./cypress/plugins/index.ts").default(on, config);
     },
