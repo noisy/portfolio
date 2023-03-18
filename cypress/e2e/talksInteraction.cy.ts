882826d23279349b330b5c3e566ba7f389fb4bab
Marek Szumny <lukmarcus@gmail.com>
2023-03-18 23:47:13 +0100

    refactor of talksInteraction with contexts

--- cypress/e2e/talksInteraction.cy.ts
+++ cypress/e2e/talksInteraction.cy.ts
@@ -232,8 +232,273 @@ describe("Talks page correct content", () => {
     );
   });
 
-  it("Given talks from particular category, it should display proper items in regards of selected language filter", () => {
-    ////////// FOR ALL talk category: *, ENGLISH, POLISH
+  context(
+    "Testing visability of items against combinations of Talk filters and Language Filter",
+    () => {
+      context(
+        "Testing visability of items in 'All' category against combinations of language Filter",
+        () => {
+          beforeEach(() => {
+            assertFiltersWorksCorrectly("#talk-filters", "*", [
+              "git",
+              "docker",
+              "blockchain",
+            ]);
+          });
+
+          it("Should display proper items in 'All' category and 'All' language filter", () => {
+            assertFiltersWorksCorrectly("#language-filters", "*", [
+              "english",
+              "polish",
+            ]);
+            assertVisabilityOfItems(
+              [
+                "bitcoins-en",
+                "blockchain1-pl",
+                "blockchain2-pl",
+                "docker-pl",
+                "git-pl",
+                "lightning-en",
+                "steem-pl",
+              ],
+              []
+            );
+          });
+
+          it("Should display proper items in 'All' category and 'English' language filter", () => {
+            assertFiltersWorksCorrectly("#language-filters", "english", [
+              "*",
+              "polish",
+            ]);
+            assertVisabilityOfItems(
+              ["bitcoins-en", "lightning-en"],
+              [
+                "blockchain1-pl",
+                "blockchain2-pl",
+                "docker-pl",
+                "git-pl",
+                "steem-pl",
+              ]
+            );
+          });
+
+          it("Should display proper items in 'All' category and 'Polish' language filter", () => {
+            assertFiltersWorksCorrectly("#language-filters", "polish", [
+              "*",
+              "english",
+            ]);
+            assertVisabilityOfItems(
+              [
+                "blockchain1-pl",
+                "blockchain2-pl",
+                "docker-pl",
+                "git-pl",
+                "steem-pl",
+              ],
+              ["bitcoins-en", "lightning-en"]
+            );
+          });
+        }
+      );
+
+      context(
+        "Testing visability of items in 'Git' category against combinations of language Filter",
+        () => {
+          beforeEach(() => {
+            assertFiltersWorksCorrectly("#talk-filters", "git", [
+              "*",
+              "docker",
+              "blockchain",
+            ]);
+          });
+
+          it("Should display proper items in 'Git' category and 'All' language filter", () => {
+            assertFiltersWorksCorrectly("#language-filters", "*", [
+              "english",
+              "polish",
+            ]);
+            assertVisabilityOfItems(
+              ["git-pl"],
+              [
+                "bitcoins-en",
+                "blockchain1-pl",
+                "blockchain2-pl",
+                "docker-pl",
+                "lightning-en",
+                "steem-pl",
+              ]
+            );
+          });
+
+          it("Should display proper items in 'Git' category and 'English' language filter", () => {
+            assertFiltersWorksCorrectly("#language-filters", "english", [
+              "*",
+              "polish",
+            ]);
+            assertVisabilityOfItems(
+              [],
+              [
+                "bitcoins-en",
+                "blockchain1-pl",
+                "blockchain2-pl",
+                "docker-pl",
+                "git-pl",
+                "lightning-en",
+                "steem-pl",
+              ]
+            );
+          });
+
+          it("Should display proper items in 'Git' category and 'Polish' language filter", () => {
+            assertFiltersWorksCorrectly("#language-filters", "polish", [
+              "*",
+              "english",
+            ]);
+            assertVisabilityOfItems(
+              ["git-pl"],
+              [
+                "bitcoins-en",
+                "blockchain1-pl",
+                "blockchain2-pl",
+                "docker-pl",
+                "lightning-en",
+                "steem-pl",
+              ]
+            );
+          });
+        }
+      );
+
+      context(
+        "Testing visability of items in 'Docker' category against combinations of language Filter",
+        () => {
+          beforeEach(() => {
+            assertFiltersWorksCorrectly("#talk-filters", "docker", [
+              "*",
+              "git",
+              "blockchain",
+            ]);
+          });
+
+          it("Should display proper items in 'Docker' category and 'All' language filter", () => {
+            assertFiltersWorksCorrectly("#language-filters", "*", [
+              "english",
+              "polish",
+            ]);
+            assertVisabilityOfItems(
+              ["docker-pl"],
+              [
+                "bitcoins-en",
+                "blockchain1-pl",
+                "blockchain2-pl",
+                "git-pl",
+                "lightning-en",
+                "steem-pl",
+              ]
+            );
+          });
+
+          it("Should display proper items in 'Docker' category and 'English' language filter", () => {
+            assertFiltersWorksCorrectly("#language-filters", "english", [
+              "*",
+              "polish",
+            ]);
+            assertVisabilityOfItems(
+              [],
+              [
+                "bitcoins-en",
+                "blockchain1-pl",
+                "blockchain2-pl",
+                "docker-pl",
+                "git-pl",
+                "lightning-en",
+                "steem-pl",
+              ]
+            );
+          });
+
+          it("Should display proper items in 'Docker' category and 'Polish' language filter", () => {
+            assertFiltersWorksCorrectly("#language-filters", "polish", [
+              "*",
+              "english",
+            ]);
+            assertVisabilityOfItems(
+              ["docker-pl"],
+              [
+                "bitcoins-en",
+                "blockchain1-pl",
+                "blockchain2-pl",
+                "git-pl",
+                "lightning-en",
+                "steem-pl",
+              ]
+            );
+          });
+        }
+      );
+
+      context(
+        "Testing visability of items in 'Blockchain' category against combinations of language Filter",
+        () => {
+          beforeEach(() => {
+            assertFiltersWorksCorrectly("#talk-filters", "blockchain", [
+              "*",
+              "docker",
+              "git",
+            ]);
+          });
+
+          it("Should display proper items in 'Blockchain' category and 'All' language filter", () => {
+            assertFiltersWorksCorrectly("#language-filters", "*", [
+              "english",
+              "polish",
+            ]);
+            assertVisabilityOfItems(
+              [
+                "bitcoins-en",
+                "blockchain1-pl",
+                "blockchain2-pl",
+                "lightning-en",
+                "steem-pl",
+              ],
+              ["docker-pl", "git-pl"]
+            );
+          });
+
+          it("Should display proper items in 'Blockchain' category and 'English' language filter", () => {
+            assertFiltersWorksCorrectly("#language-filters", "english", [
+              "*",
+              "polish",
+            ]);
+            assertVisabilityOfItems(
+              ["bitcoins-en", "lightning-en"],
+              [
+                "blockchain1-pl",
+                "blockchain2-pl",
+                "docker-pl",
+                "git-pl",
+                "steem-pl",
+              ]
+            );
+          });
+
+          it("Should display proper items in 'Blockchain' category and 'Polish' language filter", () => {
+            assertFiltersWorksCorrectly("#language-filters", "polish", [
+              "*",
+              "english",
+            ]);
+            assertVisabilityOfItems(
+              ["blockchain1-pl", "blockchain2-pl", "steem-pl"],
+              ["bitcoins-en", "docker-pl", "git-pl", "lightning-en"]
+            );
+          });
+        }
+      );
+    }
+  );
+
+  it("Given talks from particular category, it Should display proper items in regards of selected language filter", () => {
+    ////////// FOR 'All' talk category: *, ENGLISH, POLISH
     assertFiltersWorksCorrectly("#talk-filters", "*", [
       "git",
       "docker",
