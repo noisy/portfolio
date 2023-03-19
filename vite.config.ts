1497f9fe6b7344de7308ebedbb0b25190e0ca0bd
Marek Szumny <lukmarcus@gmail.com>
2023-03-19 01:10:40 +0100

    baseurl set to /

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio-marka/",
+  base: "/",
   plugins: [vue()],
   resolve: {
     alias: {
