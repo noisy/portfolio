6d82cbe2fcf395760141c73a355647b4a298e074
Marek Szumny <lukmarcus@gmail.com>
2023-04-13 11:49:04 +0200

    fontAwesome update in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import fontAwesomePlugin from "./fontawesome";
-import router from "./router";
+/* import fontAwesomePlugin from "./fontawesome"; */
+/* import router from "./router"; */
 
-createApp(App).use(router).use(fontAwesomePlugin).mount("#app");
+/* createApp(App).use(router).use(fontAwesomePlugin).mount("#app"); */
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faUserSecret);
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
